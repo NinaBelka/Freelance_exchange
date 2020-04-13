b63dd9a6afe7e7ed6f3d35a54ee00a8d3088f431
“NINA_VOLCHEK” <mysha76@mail.ru>
2020-04-13 20:53:48 +0300

    Реализовано заполнение модального окна. реализовано закрытие модального окна

--- script/script.js
+++ script/script.js
@@ -12,7 +12,16 @@ document.addEventListener('DOMContentLoaded', () => {
     modalOrder = document.getElementById('order_read'),
     modalOrderActive = document.getElementById('order_active');
 
-  const orders = [];
+  const orders = JSON.parse(localStorage.getItem('freeOrders')) || [];
+
+  const toStorage = () => {
+    localStorage.setItem('freeOrders', JSON.stringify(orders));
+  };
+
+  const calcDeadline = (deadline) => {
+    const day = '10 дней';
+    return day;
+  };
 
   const renderOrders = () => {
 
@@ -21,33 +30,82 @@ document.addEventListener('DOMContentLoaded', () => {
     orders.forEach((order, i) => {
 
       ordersTable.innerHTML += `
-        <tr class="order" data-number-order="${i}">
+        <tr class="order ${order.active ? 'taken' : ''}" data-number-order="${i}">
           <td>${i + 1}</td>
           <td>${order.title}</td>
           <td class="${order.currency}"></td>
-          <td>${order.deadline}</td>
+          <td>${calcDeadline(order.deadline)}</td>
         </tr>
       `;
     });
   };
 
+  const handlerModal = (event) => {
+    const target = event.target;
+    const modal = target.closest('.order-modal');
+    const order = orders[modal.id];
+
+    const baseAction = () => {
+      modal.style.display = 'none';
+      toStorage();
+      renderOrders();
+    };
+
+    if (target.closest('.close') || target === modal) {
+      modal.style.display = 'none';
+    }
+
+    if (target.classList.contains('get-order')) {
+      order.active = true;
+      baseAction();
+    }
+
+    if (target.id === 'capitulation') {
+      order.active = false;
+      baseAction();
+    }
+
+    if (target.id === 'ready') {
+      orders.splice(orders.indexOf(order), 1);
+      baseAction();
+    }
+  };
+
   const openModal = (numberOrder) => {
     const order = orders[numberOrder];
-    const modal = order.active ? modalOrderActive : modalOrder;
 
-    const titleBlock = document.querySelector('.modal-title'),
-      firstNameBlock = document.querySelector('.firstName'),
-      emailBlock = document.querySelector('.email'),
-      descriptionBlock = document.querySelector('.description'),
-      deadlineBlock = document.querySelector('.deadline'),
-      currencyBlock = document.querySelector('.currency_img'),
-      countBlock = document.querySelector('.count'),
-      phoneBlock = document.querySelector('.phone');
-    
-    titleBlock.textContent = order.title;
-    
+    const { title, firstName, email, phone, description, amount, currency, deadline, active = false } = order;
 
-    modal.style.display = 'block';
+    const modal = active ? modalOrderActive : modalOrder;
+
+
+    const titleBlock = modal.querySelector('.modal-title'),
+      firstNameBlock = modal.querySelector('.firstName'),
+      emailBlock = modal.querySelector('.email'),
+      descriptionBlock = modal.querySelector('.description'),
+      deadlineBlock = modal.querySelector('.deadline'),
+      currencyBlock = modal.querySelector('.currency_img'),
+      countBlock = modal.querySelector('.count'),
+      phoneBlock = modal.querySelector('.phone');
+
+    modal.id = numberOrder;
+    titleBlock.textContent = title;
+    firstNameBlock.textContent = firstName;
+    emailBlock.textContent = email;
+    emailBlock.href = 'mailto:' + email;
+    descriptionBlock.textContent = description;
+    deadlineBlock.textContent = calcDeadline(deadline);
+    currencyBlock.className = 'currency_img';
+    currencyBlock.classList.add(currency);
+    countBlock.textContent = amount;
+
+    phoneBlock && (phoneBlock.href = 'tel:' + phone);
+
+    modal.style.display = 'flex';
+
+    modal.addEventListener('click', handlerModal);
+
+
   };
 
   ordersTable.addEventListener('click', event => {
@@ -97,6 +155,7 @@ document.addEventListener('DOMContentLoaded', () => {
     formCustomer.reset();
 
     orders.push(obj);
+    toStorage();
   });
 
 
